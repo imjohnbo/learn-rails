3e18886ab5f166b61fbd574f3e70298ff044eccb
John Bohannon <imjohnbo@github.com>
2022-06-20 17:20:52 +0000

    feat: variable pages titles using provide and yield

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'static_pages/about'
 end
